fe3b5a934b27019baef5d20fe8af43394076fe7c
Catalin Blaga <catalin449@gmail.com>
2024-03-25 18:34:49 +0000

    added new features

--- main.py
+++ main.py
@@ -2,9 +2,14 @@
 # if player, then he can play, if admin, he can add questions
 
 import users
+import game
 
 if __name__ == '__main__':
     welcome_msg = "Welcome to Quiz Game!"
     print(f"{len(welcome_msg) * '='}{welcome_msg}{len(welcome_msg) * '='}")
 
-    users.login()
+    current_player = users.login()
+
+    while True:
+        print(f"Let's play {list(current_player.keys())[0]}")
+        game.run_game(current_player)
